7ca1cac78231b4665ac859aafc87c6b5dba51841
Tejaswini Sudhakar <tejaswanisudhakar@gmail.com>
2023-02-20 11:25:42 +0530

    Add files via upload

---
+++ Data_Analyser-main/app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import Home
+import Visualization
+import Dashboard
+
+
+st.set_page_config(layout="wide")
+s = st.button("Home")
+PAGES = {
+    "Home": Home,
+    "Visualizer": Visualization,
+    "Dashboard": Dashboard,
+
+}
+
+st.image("auto-ra-banner.jpg")
+if s == True:
+    selection = st.selectbox("Choose one", list(PAGES.keys()), key ='2')
+    Home.app()
+else:
+    selection = st.selectbox("Choose one", list(PAGES.keys()), key ='3')
+    page = PAGES[selection]
+    page.app()
